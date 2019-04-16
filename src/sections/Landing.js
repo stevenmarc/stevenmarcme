4d452fe40ee5242e8bdfba4192111ee6281d804c
stevenmarc <stevenmarcfr@gmail.com>
2019-04-16 15:57:55 +0200

    Replace grid with flex

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import { Heading, Flex, Box, Text } from 'rebass'
 import Section from '../components/Section'
@@ -15,9 +15,6 @@ const bgImageStyle = {
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
   backgroundAttachment: 'fixed',
-  display: 'grid',
-  gridTemplateColumns: 'minmax(200px, 600px) 1fr',
-  gridTemplateRows: '100px auto',
 }
 
 export default () => (
@@ -37,42 +34,31 @@ export default () => (
         const { title, body } = data.file.childDataJson
 
         return (
-          <div style={bgImageStyle}>
+          <Flex justifyContent="left" flexWrap="wrap" style={bgImageStyle}>
             <Heading
-              textAlign="left"
               as="h2"
+              textAlign="left"
               color="white"
               fontFamily="Quicksand, sans-serif"
               fontWeight="300"
               fontSize={[3, 4, 5]}
-              mb={[3, 4, 5]}
               padding="20% 10% 10% 10%"
             >
               {title}
             </Heading>
-            <div />
             <Heading
               as="h3"
               color="white"
               fontSize={[2, 3, 4]}
-              mb={[3, 4, 5]}
               textAlign="left"
+              fontFamily="Roboto, sans-serif"
+              fontWeight="100"
+              padding="20% 10% 10% 10%"
+              width={[300, 500]}
             >
-              <div>
-                <Text
-                  width={[300, 500]}
-                  style={{
-                    fontFamily: 'Roboto, sans-serif',
-                    fontWeight: 100,
-                    fontSize: '1.5em',
-                    padding: '20% 10% 0% 10%',
-                    color: 'white',
-                  }}
-                  dangerouslySetInnerHTML={{ __html: body }}
-                />
-              </div>
+              <Text dangerouslySetInnerHTML={{ __html: body }} />
             </Heading>
-          </div>
+          </Flex>
         )
       }}
     />
