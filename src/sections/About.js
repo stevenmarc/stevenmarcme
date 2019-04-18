29a05b903a21ed0e8f0c48b951bf23714549ea26
stevenmarc <stevenmarcfr@gmail.com>
2019-04-18 13:04:02 +0200

    Add about content

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,10 +1,156 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Section from '../components/Section'
+import { StaticQuery, graphql } from 'gatsby'
+import { Heading, Flex, Box, Text } from 'rebass'
 
 const About = () => (
   <Section.Container id="about">
-    <Section.Header name="About me" icon="🙋‍♂️" label="person" />
-    <span>About</span>
+    <StaticQuery
+      query={graphql`
+        query AboutSectionQuery {
+          file(name: { eq: "about" }) {
+            childDataJson {
+              title
+              bodies {
+                title
+                description
+                descriptions {
+                  title
+                  description
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const { title, bodies } = data.file.childDataJson
+        return (
+          <Fragment>
+            <Section.Header name="About me" icon="🙋‍♂️" label="person" />
+            <Flex m="10px" justifyContent="left" flexWrap="wrap">
+              <Box width={[1, 1, 1 / 2]} p={2}>
+                <Heading
+                  as="h2"
+                  textAlign="left"
+                  color="black"
+                  fontFamily="Quicksand, sans-serif"
+                  fontWeight="300"
+                  fontSize={[3, 4, 5]}
+                  padding="0.5em 1em 1em 1em"
+                >
+                  {bodies[0].title}
+                </Heading>
+                <Heading
+                  as="h3"
+                  color="black"
+                  fontSize={[2, 3, 4]}
+                  textAlign="left"
+                  fontFamily="Roboto, sans-serif"
+                  fontWeight="100"
+                  padding="0.5em 1em 1em 1em"
+                >
+                  <Text
+                    dangerouslySetInnerHTML={{ __html: bodies[0].description }}
+                  />
+                </Heading>
+              </Box>
+              <Box width={[1, 1, 1 / 2]} p={2}>
+                <Heading
+                  as="h2"
+                  textAlign="left"
+                  color="black"
+                  fontFamily="Quicksand, sans-serif"
+                  fontWeight="300"
+                  fontSize={[3, 4, 5]}
+                  padding="1em 1em 1em 1em"
+                >
+                  {bodies[1].title}
+                </Heading>
+                <Heading
+                  as="h2"
+                  textAlign="left"
+                  color="black"
+                  fontFamily="Quicksand, sans-serif"
+                  fontWeight="300"
+                  fontSize={[2, 3, 4]}
+                  padding="1em 1em 1em 1em"
+                >
+                  {bodies[1].descriptions[0].title}
+                </Heading>
+                <Heading
+                  as="h3"
+                  color="black"
+                  fontSize={[2, 3, 4]}
+                  textAlign="left"
+                  fontFamily="Roboto, sans-serif"
+                  fontWeight="100"
+                  padding="1em"
+                >
+                  <Text
+                    dangerouslySetInnerHTML={{
+                      __html: bodies[1].descriptions[0].description,
+                    }}
+                  />
+                </Heading>
+                <Heading
+                  as="h2"
+                  textAlign="left"
+                  color="black"
+                  fontFamily="Quicksand, sans-serif"
+                  fontWeight="300"
+                  fontSize={[2, 3, 4]}
+                  padding="1em 1em 1em 1em"
+                >
+                  {bodies[1].descriptions[1].title}
+                </Heading>
+                <Heading
+                  as="h3"
+                  color="black"
+                  fontSize={[2, 3, 4]}
+                  textAlign="left"
+                  fontFamily="Roboto, sans-serif"
+                  fontWeight="100"
+                  padding="1em"
+                >
+                  <Text
+                    dangerouslySetInnerHTML={{
+                      __html: bodies[1].descriptions[1].description,
+                    }}
+                  />
+                </Heading>
+                <Heading
+                  as="h2"
+                  textAlign="left"
+                  color="black"
+                  fontFamily="Quicksand, sans-serif"
+                  fontWeight="300"
+                  fontSize={[2, 3, 4]}
+                  padding="1em 1em 1em 1em"
+                >
+                  {bodies[1].descriptions[2].title}
+                </Heading>
+                <Heading
+                  as="h3"
+                  color="black"
+                  fontSize={[2, 3, 4]}
+                  textAlign="left"
+                  fontFamily="Roboto, sans-serif"
+                  fontWeight="100"
+                  padding="1em"
+                >
+                  <Text
+                    dangerouslySetInnerHTML={{
+                      __html: bodies[1].descriptions[2].description,
+                    }}
+                  />
+                </Heading>
+              </Box>
+            </Flex>
+          </Fragment>
+        )
+      }}
+    />
   </Section.Container>
 )
 
